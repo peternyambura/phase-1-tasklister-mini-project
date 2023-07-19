6a60f833b28bbd256bb4041a28d2f9924f4d2702
= <peterndirangu0005@gmail.com>
2023-07-19 21:44:04 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskDescription = document.getElementById("new-task-description").value;
+    createTask(taskDescription);
+    form.reset();
+  });
+
+  function createTask(description) {
+    const task = document.createElement("li");
+    task.innerText = description;
+    taskList.appendChild(task);
+  }
 });
